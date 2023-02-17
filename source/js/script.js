643bd5b092618a0fd5e7375a53068b7c104e2311
Artur <alimayko91@gmail.com>
2023-02-17 19:07:40 +0300

    сделал слайдер, начал делать ретинизацию

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,12 @@
-/* в этот файл добавляет скрипты*/
+new Swiper(".swiper", {
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable:true,
+  },
+});
