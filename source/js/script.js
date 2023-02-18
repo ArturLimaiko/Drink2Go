b1ceb03f837d2978d4db47d38a6ddc0ad61e4c40
Artur <alimayko91@gmail.com>
2023-02-19 00:53:05 +0300

    сделал бургер меню, добавил карту , сделал рендж с ценой

--- source/js/script.js
+++ source/js/script.js
@@ -1,12 +1,56 @@
 new Swiper(".swiper", {
+  loop: true,
 
   navigation: {
     nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev"
+    prevEl: ".swiper-button-prev",
   },
 
   pagination: {
     el: ".swiper-pagination",
-    clickable:true,
+    clickable: true,
   },
 });
+
+const navToggler = document.querySelector(".main-nav__toggle");
+const navigation = document.querySelector(".main-nav__list");
+
+navToggler.hidden = false;
+navigation.classList.add("main-nav__list--enabled");
+
+navToggler.addEventListener("click", () => {
+  navigation.classList.toggle("main-nav__list--opened");
+  navToggler.classList.toggle("main-nav__toggle--opened");
+});
+
+
+const map = L.map('map')
+    .setView({
+      lat: 59.968321,
+      lng: 30.317462,
+    }, 17);
+
+  L.tileLayer(
+    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    },
+  ).addTo(map);
+
+  const mainPinIcon = L.icon({
+    iconUrl: '../img/map-pin.svg',
+    iconSize: [38, 50],
+    iconAnchor: [16, 50],
+  });
+
+  const marker = L.marker(
+    {
+      lat: 59.968321,
+      lng: 30.317462,
+    },
+    {
+      icon: mainPinIcon,
+    },
+  );
+
+  marker.addTo(map);
