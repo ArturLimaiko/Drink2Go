0cbf26e2c621369b8f2397d5f126a4864579030a
Artur <alimayko91@gmail.com>
2023-02-21 02:00:55 +0300

    доделал состояни у всех элементо, и куча мелких правок - правка верстки по бэм и правка блоков + удалил не нужные классы и добавил модификаторы

--- source/js/script.js
+++ source/js/script.js
@@ -23,34 +23,39 @@ navToggler.addEventListener("click", () => {
   navToggler.classList.toggle("main-nav__toggle--opened");
 });
 
+const map = L.map("map").setView(
+  {
+    lat: 59.968321,
+    lng: 30.317462,
+  },
+  17
+);
 
-const map = L.map('map')
-    .setView({
-      lat: 59.968321,
-      lng: 30.317462,
-    }, 17);
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
 
-  L.tileLayer(
-    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-    {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    },
-  ).addTo(map);
+const mainPinIcon = L.icon({
+  iconUrl: "../img/map-pin.svg",
+  iconSize: [38, 50],
+  iconAnchor: [16, 50],
+});
 
-  const mainPinIcon = L.icon({
-    iconUrl: '../img/map-pin.svg',
-    iconSize: [38, 50],
-    iconAnchor: [16, 50],
-  });
+const marker = L.marker(
+  {
+    lat: 59.968321,
+    lng: 30.317462,
+  },
+  {
+    icon: mainPinIcon,
+  }
+);
 
-  const marker = L.marker(
-    {
-      lat: 59.968321,
-      lng: 30.317462,
-    },
-    {
-      icon: mainPinIcon,
-    },
-  );
+marker.addTo(map);
 
-  marker.addTo(map);
+const mapBox = document.querySelector(".map__box");
+mapBox.classList.remove("map__no-js");
+
+const header = document.querySelector(".header");
+header.classList.remove("header--no-js");
